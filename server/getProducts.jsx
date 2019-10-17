c6e8d8d986326fb37a7908cbd33ec540875a4cc2
WillHoll <willhollingsworth15@yahoo.com>
2019-10-17 15:48:48 -0600

    complete

--- server/getProducts.jsx
+++ server/getProducts.jsx
@@ -1,6 +1,11 @@
 const products = require('../products.json');
 
 const getProducts = (req, res) => {
-    res.status(200).send(products);
+    const price = products.filter(product => product.price >= Number(req.query.price));
+    if (!req.query.price) {
+        return res.status(200).send(products);
+    } else {
+        return res.status(200).send(price);
+    }
 }
     module.exports = getProducts;
